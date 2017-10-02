fc3e505e5ad65d3d7f8d4b3b51f54e5dcb67dded
Aaron Loar <aaronloar@gmail.com>
2017-10-02 16:35:08 -0700

    Changed around the layout / display on the html page.

--- stockapp/views/view_ten_mm.py
+++ stockapp/views/view_ten_mm.py
@@ -1,8 +1,9 @@
-import pprint
 
-import sys
-import requests
 from flask import render_template
+import pprint
+import requests
+import sys
+import time
 
 import av_calls
 from stockapp import app
@@ -35,52 +36,53 @@ def get_daily(symbol):
               }
 
     print("Getting info for: {}".format(symbol))
+    ts = time.time()
     r = requests.get(app.config["URL"], params=params)
 
-    if r.status_code != 200:
-        print("Error getting daily data: {}\n{}".format(r.status_code, r.text))
-        return render_template("daily.html", symbol=symbol,
-                               meta="Error code: {}".format(r.status_code),
-                               data="{}".format(r.text))
-    else:
-        meta = r.json()['Meta Data']
-        data = r.json()['Time Series (Daily)']
+    print("{} - {}".format(r.status_code, time.time() - ts))
+    r.raise_for_status()
 
-        f_meta = pprint.pformat(meta, indent=2, width=100)
-        f_data = pprint.pformat(data, indent=2, width=70)
+    meta = r.json()['Meta Data']
+    data = r.json()['Time Series (Daily)']
 
-        # close, close_date = calculations.close_price(symbol)
+    f_meta = pprint.pformat(meta, indent=2, width=100)
+    f_data = pprint.pformat(data, indent=2, width=70)
 
-        close_date = sorted(data.keys())[-1]
-        close = data[close_date]
-        close_prev = float(data[sorted(data.keys())[-2]]["4. close"])
+    # close, close_date = calculations.close_price(symbol)
 
-        for k, v in close.items():
-            close[k] = float(v)
+    close_date = sorted(data.keys())[-1]
+    close = data[close_date]
+    close_prev = float(data[sorted(data.keys())[-2]]["4. close"])
 
-        sma = calculations.sma(data, sma_period)
-        atr = calculations.atr(data, atr_period)
-        rsi = calculations.rsi(data, rsi_period)
-        low3 = calculations.low3(data)
-        stop = calculations.get_stop(low3, atr)
-        max_loss = close["4. close"] - stop
+    for k, v in close.items():
+        close[k] = float(v)
 
-        info = {"symbol": symbol,
-                "meta": f_meta,
-                "data": f_data,
-                "close_price": "{:.2f}".format(close["4. close"]),
-                "close_change": "{:.2f}".format(close["4. close"] - close_prev),
-                "close_date": close_date,
-                "c_sma": "{:.3f}".format(sma),
-                "c_rsi": "{:.3f}".format(rsi),
-                "c_atr": "{:.4f}".format(atr),
-                "c_low3": "{:.2f}".format(low3),
-                "c_stop": "{:.2f}".format(stop),
-                "c_max_loss": "{:.2f}".format(max_loss)
+    ts = time.time()
 
-                }
+    sma = calculations.sma(data, sma_period)
+    atr = calculations.atr(data, atr_period)
+    rsi = calculations.rsi(data, rsi_period)
+    low3 = calculations.low3(data)
+    stop = calculations.get_stop(low3, atr)
+    max_loss = close["4. close"] - stop
+    print("Calculation time: {}".format(time.time() - ts))
 
-        return render_template("daily.html", info=info)
+    info = {"symbol": symbol,
+            "meta": f_meta,
+            "data": f_data,
+            "close_price": "{:.2f}".format(close["4. close"]),
+            "close_change": "{:.2f}".format(close["4. close"] - close_prev),
+            "close_date": close_date,
+            "c_sma": "{:.3f}".format(sma),
+            "c_rsi": "{:.3f}".format(rsi),
+            "c_atr": "{:.4f}".format(atr),
+            "c_low3": "{:.2f}".format(low3),
+            "c_stop": "{:.2f}".format(stop),
+            "c_max_loss": "{:.2f}".format(max_loss)
+
+            }
+
+    return render_template("daily.html", info=info)
 
 
 if __name__ == '__main__':
